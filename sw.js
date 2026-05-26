26c6b56d4e6f8045e632bcfd40ff5897b9876112
Hamad77S <surour77h@gmail.com>
2026-05-26 20:37:18 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='hmd-lego-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./data.json','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
